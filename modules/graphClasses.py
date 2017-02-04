649f3298dbeb92bee678ceb997dd0116d8770fe0
stefano <stefano@debian>
2017-02-04 21:48:17 +0000

    additions

--- modules/graphClasses.py
+++ modules/graphClasses.py
@@ -1,6 +1,7 @@
 from collections import defaultdict
 
 
+
 class EntityCollection(object):
     
     
@@ -16,10 +17,12 @@ class EntityCollection(object):
     
     def getEntity(self, entity_name):
         ''' '''
-        pass
+        return self._EntityRegistry[entity_name]
     
     
-    def removeEntity(self, entity_name):
+    def removeEntity(self, entity):
+        ''' '''
+        del self._EntityRegistry[entity.name]
         
 
 
@@ -31,9 +34,14 @@ class EntityTypes(object):
         self._collections = defaultdict(EntityCollection)
         
         
-    def addEntity(self, new_type, new_entity):
+    def addEntity(self, new_entity):
         ''' '''
-        self._collections[new_type].addEntity(new_entity)
+        self._collections[new_entity.type].addEntity(new_entity)
+        
+        
+    def removeEntity(self, entity):
+        ''' '''
+        self._collections[entity.type].removeEntity(entity)
     
 
 
@@ -43,39 +51,30 @@ class Field(object):
     def __init__(self):
         ''' The field is the space that holds and manages all other classes. From the field is possible
         to search other create, instances and modify them. '''
-        self._entityRegistry = {} # all entities are kept in this dictionary, searchable by type and name
+        self._entity_reg = EntityTypes() # all entities are kept in this dictionary, searchable by type and name
         
         
-    def getEntity(self, eType, eValue):
-        ''' Lookup en existing entity '''
+    def addLink(self, entity_src, entity_dst):
+        ''' Create an undirected edge between two entities '''
         pass
         
         
-    def createEntity(self, eType, eValue):
+    def createEntity(self, entity_type, entity_name):
         ''' Create a new entity '''
-        newEntity = Entity(eType, eValue, self)
-        return newEntity
+        new_entity = Entity(entity_type, entity_name, self)
+        self._entity_reg.addEntity(new_entity)
+        return new_entity
         
         
 
 class Entity(object):
     
     
-    def __init__(self, entType, entValue, entField):
-        if isinstance(entField, Field):
-            self.type = entType
-            self.value = entValue
-            self.field = entField
-            self.group = None
-            self.links = WeakValueDictionary() # dict of linked entities
-            self.field.registerEntity(self) # update the entity registry
-        else:
-            raise TypeError("Invalid field argument, field instance expected!")
-    
-    
-    def linkTo(self, eTwo):
-        ''' Linking operation is bi-directional, affects both entities equally.'''
-        pass
+    def __init__(self, entity_type, entity_name):
+        self.type = entity_type
+        self.value = entity_name
+        self.group = None
+        self.links = WeakValueDictionary() # dict of linked entities
     
     
     def getLinks(self):
